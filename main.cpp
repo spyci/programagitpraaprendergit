c4596c413b305a6937c815da101406342ee93cc7
ffullm <thiagovieira2609@gmail.com>
2019-11-13 02:33:37 -0300

    primeiro commit fodasse

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(int argc, char *argv[]){
+	string s;	
+	//Verifica se git está instalado
+	do{
+	cout<<"Bem vindo ao Tutorial de GIT\n"<<
+	"Verifique se git está instalado e pronto digitando <<git version>>"<<endl;
+	cin>>s;
+	} while(s!="git version");
+	system("git version");
+	//Primeira Questão
+	do{
+	" Antes de começar, vamos definir seu usuario\n"<<
+	" digite: <<git config --global user.name \"seu nome\">>"<<endl;
+	cin>>s;
+	} while (s!="git config
+		
+}
